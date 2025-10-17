ce7ee080339d1db37c938b850ba76f8dcb08d1e7
estudiante <estudiante@oretania-PC>
2025-10-17 09:10:53 +0200

    Version 1.1

--- PruebaEclipse/src/PruebaEclipse/Adios.java
+++ PruebaEclipse/src/PruebaEclipse/Adios.java
@@ -3,6 +3,7 @@ package PruebaEclipse;
 public class Adios {
 	public static void main(String[] args) {
 		
+		
 		System.out.println("¡Adios, Mundo!");
 	}
 }
--- PruebaEclipse/src/PruebaEclipse/Prueba.java
+++ PruebaEclipse/src/PruebaEclipse/Prueba.java
@@ -5,7 +5,6 @@ public class Prueba {
 	public static void main(String[] args) {
 		
 			System.out.println("¡Hola, Mundo!");
-			
 }
 
 }
